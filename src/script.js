0cae15b07d5fff80e8f869abf7b4284bd2a96e08
umbara-web <umbara81.web@gmail.com>
2025-08-03 22:11:39 +0700

    revice : navigation, hero section and addition features who we are section

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ const humberger = document.querySelector('.humberger');
 const menu = document.querySelector('.menu');
 
 humberger.addEventListener('click', function () {
-  let src = humberger.src.includes('humberger')
+  let src = humberger.src.includes('menu-light')
     ? './asset/images/x-close.svg'
     : './asset/images/menu-light.svg';
   humberger.src = src;
